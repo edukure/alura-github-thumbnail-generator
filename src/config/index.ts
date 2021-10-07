e4437144f891d01e4fd36f99c73159404dc09e87
Eduardo Tioma <tioma.eduardo@gmail.com>
2021-10-07 12:43:21 -0300

    consertar erro de configuração

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,3 @@
-export const BASE_URL =
-  `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` || 'http://localhost:3000';
+export const BASE_URL = process.env.NEXT_PUBLIC_VERCEL_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+  : 'http://localhost:3000';
